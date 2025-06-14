0cc7f903803053aa22bf84b7f1e42a9ec43ab273
Stefan Haas <stefan.haas.privat@gmail.com>
2025-06-14 22:56:01 +0200

    improve file parser

--- analyzer/src/core/parser/file.rs
+++ analyzer/src/core/parser/file.rs
@@ -1,17 +1,16 @@
 use std::fs;
+use std::io;
 
 pub struct ParsedFile {
     pub path: String,
     pub content: String,
 }
 
-pub fn parse(path: &str) -> Result<ParsedFile, String> {
-    let content = fs::read_to_string(path);
-    match content {
-        Ok(content) => Ok(ParsedFile {
-            path: path.to_owned(),
-            content: content.clone(),
-        }),
-        Err(e) => Err(format!("Failed to read file {}: {}", path, e)),
-    }
+pub fn parse(path: &str) -> io::Result<ParsedFile> {
+    let content = fs::read_to_string(path)?;
+
+    Ok(ParsedFile {
+        path: path.to_owned(),
+        content: content,
+    })
 }
